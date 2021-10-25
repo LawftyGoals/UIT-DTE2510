635c004ad1de3483c5d8d8b317960ae1c06f35a8
Oliver Sokol <oliver.sokol@atea.no>
2021-10-25 11:09:59 +0200

    Updated look at bean_machine

--- DTE2510-O3_Bean_machine.py
+++ DTE2510-O3_Bean_machine.py
@@ -1,24 +1,34 @@
 import random
 
-def in_motion (slotsNr: int, balls: int):
-    slots = [0]*slotsNr
+def in_motion (slots_nr: int, balls: int):
+    slots = [0]*slots_nr
 
     for i in range (0, balls):
-        pathString = ""
+        path_string = ""
         path = 0
         
-        for j in range (0, slotsNr):
+        for j in range (0, slots_nr):
             chance = random.randint(0,1)
 
             if chance == 0:
-                pathString += "L"
+                path_string += "L"
             else:
                 path +=1
-                pathString += "R"
+                path_string += "R"
             
         slots[path] +=1
         
-        print(pathString)
+        print(path_string)
+
+    return slots
+
+# This could be made as it's own function. histogram_creation() function feks.
+# Black - PIP makes sure code is formatted correctly.
+# Added return statement to in_motion, this passes to slots in main(), then uses print_histogram function.
+#
+#
+
+def print_histogram(slots: list):
 
     for ii in range(max(slots)+2, 0, -1):
         
@@ -39,7 +49,10 @@ def in_motion (slotsNr: int, balls: int):
 
 
 def main():
-    in_motion(int(input("Skriv inn antall ønsker spor: ")), int(input("Skriv inn antall ønsket baller: ")))
+
+    slots = in_motion(int(input("Skriv inn antall ønsker spor: ")), int(input("Skriv inn antall ønsket baller: ")))
+    print_histogram(slots)
+    
 
 
 if __name__ == "__main__":

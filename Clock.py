a80f47a1b991067ee434f2e41031323656ee8618
LawtyG <lawtyg@pop-os.localdomain>
2021-11-19 21:45:09 +0100

    Clock, men sånn som den skal løses

--- Clock.py
+++ Clock.py
@@ -1,5 +1,4 @@
 
-
 class Clock:
     def __init__(self, day=0, month=0, year=0, hour=0, min=0, sec=0):
         self.sec = sec
@@ -9,57 +8,59 @@ class Clock:
         self.hour = hour
         self.min = min
         
-    def inc_sec(self, sec):
-        if (sec > 59):
+    def inc_sec(self, sec=1):
+        self.sec += sec
+        if (self.sec > 59):
+            self.sec %= 60
             self.min += 1
-            self.sec = sec % 60
-        else:
-            self.sec = sec
+            self.inc_min(0)
 
-    def inc_min(self, minu):
-        print(minu+self.min)
-        if (minu + self.min) > 59:
+    def inc_min(self, minu=1):
+        self.min += minu
+        if self.min > 59:
+            self.min %= 60
             self.hour += 1
-            self.min = (minu+self.min) % 60
-            print("if")
-        else:
-            self.min += minu
+            self.inc_hour(0)
 
-    def inc_hour(self, hour):
+    def inc_hour(self, hour=1):
+        self.hour += hour
         if hour + self.hour > 23:
+            self.hour %= 24
             self.day += 1
-            self.hour = (hour+self.hour) % 24
-        else:
-            self.hour += hour
+            self.inc_day(0)
     
-    def inc_day(self, day):
+    def inc_day(self, day=1):
         if (self.year % 4 == 0 and self.year % 100 != 0) or (self.year % 400 == 0):
             leap = 29
         else:
             leap = 28
-        if day + self.day > leap and self.month == 2:
+
+        self.day += day
+
+        if self.day > leap and self.month == 2:
+            self.day %= leap
             self.month += 1
-            self.day = (self.day+day) % 28
-        elif day + self.day> 30 and self.month in [4,6,9,11]:
+        elif self.day> 30 and self.month in [4,6,9,11]:
+            self.day %= 30
             self.month += 1
-            self.day = (self.day+day) % 30
-        elif day + self.day> 31:
+        elif self.day> 31:
+            self.day %= 31
             self.month += 1
-            self.day = (self.day+day) % 31
-        else:
-            self.day += day
+        self.inc_month(0)
     
 
-    def inc_month(self, month):
+    def inc_month(self, month=1):
+        self.month += month
         if month + self.month > 12:
+            self.month %= 12
             self.year += 1
-            self.month = (self.month+month) % 12
-        else:
-            self.month += month
+
+    def inc_year(self, year=1):
+        self.year += year
     
 
     def clock_as_string(self):
-        return f'{self.day:02d}:{self.month:02d}:{self.year:02d}:{self.hour:02d}:{self.min:02d}:{self.sec:02d}'
+        return f'{self.day:02d}:{self.month:02d}:{self.year:04d}:{self.hour:02d}:{self.min:02d}:{self.sec:02d}'
     
     def set_clock(self, day: int, month: int, year: int, hour: int, min: int, sec: int):
         self.inc_sec(sec)
@@ -69,15 +70,3 @@ class Clock:
         self.inc_month(month)
         self.year += year
 
-clock = Clock()
-clock.set_clock(31,12,2013,23,59,60)
-
-print(clock.clock_as_string())
-
-"""
-def main():
-
-
-
-if __name__ == '__main__':
-    main()"""
---
+++ Test_Clock.py
@@ -0,0 +1,24 @@
+from Clock import Clock
+import unittest
+
+class Test_Clock(unittest.TestCase):
+
+    def setUp(self):
+        self.__clock = Clock()
+
+    def test_inc_sec_from_default_values(self):
+        self.__clock.inc_sec()
+        self.assertEqual(self.__clock.clock_as_string(), "00:00:0000:00:00:01")
+
+    def test_inc_min_from_default_values(self):
+        self.__clock.inc_min()
+        self.assertEqual(self.__clock.clock_as_string(), "00:00:0000:00:01:00")
+    
+    def test_inc_sec_from_midnight_jan(self):
+        self.__clock = Clock(31, 1, 2021, 23, 59, 59)
+        self.__clock.inc_sec()
+        self.assertEqual(self.__clock.clock_as_string(), "01:02:2021:00:00:00")
+
+    
+if __name__ == "__main__":
+    unittest.main()
